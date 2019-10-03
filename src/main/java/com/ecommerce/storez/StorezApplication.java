30f556f2e2b485c7e48c9bcddb43fdb05b32da63
Alex Manuel Calagua Gavilan <gavilan@lnxcit019247.cit>
2019-10-03 10:41:48 -0300

    initial commit

---
+++ src/main/java/com/ecommerce/storez/StorezApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.storez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorezApplication.class, args);
+	}
+
+}
